b97d3c33bbe2778fe403d9d1a54ffef342c0020d
Jonathan Kelley <jonathan.kelley@gmail.com>
2017-01-04 20:50:51 -0600

    Stop setting searcher at initialize. Require this to be done at app level for failover

--- lib/spree_searchkick/engine.rb
+++ lib/spree_searchkick/engine.rb
@@ -15,7 +15,6 @@ module SpreeSearchkick
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-      Spree::Config.searcher_class = Spree::Search::Searchkick
     end
 
     config.to_prepare &method(:activate).to_proc
