04e178d9ea419295e0629f623503fbb4b3eb0828
Jonathan Kelley <jonathan.kelley@gmail.com>
2017-01-12 23:36:44 -0600

    Add popularity to sorts. Removed alphabetical sorts

--- lib/spree/core/searchkick_sorts.rb
+++ lib/spree/core/searchkick_sorts.rb
@@ -6,8 +6,7 @@ module Spree
           'relevance' => { sort: { _score: :desc }, label: "Relevance" },
           'price_asc' => { sort: { price: :asc }, label: "Price Low to High" },
           'price_desc' => { sort: { price: :desc }, label: "Price High to Low" },
-          'name_asc' => { sort: { name: :asc }, label: "Name A-Z" },
-          'name_desc' => { sort: { name: :desc }, label: "Name Z-A" },
+          'popularity' => { sort: { conversions: :desc }, label: 'Popularity' }
         }
       end
       def self.current_sort(params)
